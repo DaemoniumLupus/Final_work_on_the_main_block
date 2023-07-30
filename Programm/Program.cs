08b65d29d4206644164425aa6e3f93fb6102ab08
Ilya Bakushkin <37290179+DaemoniumLupus@users.noreply.github.com>
2023-07-30 16:12:23 +0300

    Add programm

---
+++ Programm/Program.cs
@@ -0,0 +1,40 @@
+﻿
+string[] massive = new string[4] { "hello", "-23", "Rupert", "12" };
+string[] bufferMassive = new string[massive.Length];
+int bufferIndex = 0;
+
+bufferMassive = Extraction(massive, bufferMassive);
+PrintMassive(bufferMassive);
+
+string[] Extraction(string[] massive, string[] bufferMassive)
+{
+  for (int i = 0; i < massive.Length; i++)
+  {
+    if (massive[i].Length <= 3)
+    {
+      bufferMassive[bufferIndex] = massive[i];
+      bufferIndex++;
+      continue;
+    }
+  }
+  return bufferMassive;
+}
+
+void PrintMassive(string[] mas)
+{
+  for (int i = 0; i < mas.Length; i++)
+  {
+    Console.Write($"{mas[i]} ");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
